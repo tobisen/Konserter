e9cfc5617405a4de4e2d628e3d952b6e3091d963
Tobias Bertilsson <tobiasbertilsson@MacBook-Air-som-tillhor-Tobias.local>
2026-04-14 20:07:07 +0200

    Fix Vercel catch-all route normalization for API endpoints

--- api/[...route].js
+++ api/[...route].js
@@ -1,5 +1,17 @@
 import { handleApiRequest } from '../server/api.js'
 
 export default async function handler(request, response) {
+  const routeValue = request.query?.route
+  const routeParts = Array.isArray(routeValue)
+    ? routeValue
+    : routeValue
+      ? [routeValue]
+      : []
+
+  const queryString = request.url?.includes('?')
+    ? request.url.slice(request.url.indexOf('?'))
+    : ''
+
+  request.url = `/api/${routeParts.join('/')}${queryString}`
   await handleApiRequest(request, response)
 }
